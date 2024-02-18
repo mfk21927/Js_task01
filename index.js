177d2ae7bf56c37172db725546711eea653c0a8b
mfk21927 <mfk21927@gmail.com>
2024-02-18 14:06:36 +0500

    Js task done

---
+++ index.js
@@ -0,0 +1,47 @@
+// task01: showing welcome message
+
+// function displayWelcomeMessage(){
+  
+//    let name=prompt("Enter Your name plx :");
+
+//    let showMessage=("Welcome,"+ name + "!");
+
+//    alert(showMessage);
+// }
+
+// displayWelcomeMessage();
+
+
+
+// task 01; quotesGenerator
+
+
+// function generateQuote(){
+//     let aqwalEzareen=["silence is gold...", "Spread love everywhere you go.","All that glitters is not gold.","Dream big,work hard,stay focussed","Every moment is a fresh beginning."]
+
+//     const random= Math.floor(Math.random() * aqwalEzareen.length);
+  
+//   //   console.log(random);
+  
+//   let show=aqwalEzareen[random];
+//   console.log(show);
+// }
+
+// generateQuote();
+// generateQuote();
+// generateQuote();
+
+
+// random number generator
+
+function generateRandomNumber(){
+
+        const random= Math.floor(Math.random() * 100)
+        console.log(random);
+
+}
+
+generateRandomNumber();
+generateRandomNumber();
+generateRandomNumber();
+generateRandomNumber();
